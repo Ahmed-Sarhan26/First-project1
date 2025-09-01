73cd6e20fd1d477996fcadb0ff1773bac9e12c70
Ahmed Sarhan <ahmed.a.sarhan.26@gmail.com>
2025-09-01 17:28:30 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Home from './components/Home'
 import About from './components/About'
+import Contact from './components/About'
 function App() {
   return (
     <>
@@ -12,3 +13,4 @@ function App() {
 }
 
 export default App
+
