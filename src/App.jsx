2fd6d85a8537f406be1cb692d3494e2d8f85d008
Ahmed Sarhan <ahmed.a.sarhan.26@gmail.com>
2025-09-01 16:39:21 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import './App.css'
 import Home from './components/Home'
 import About from './components/About'
-import Contact from './components/Contact'
-import Products from './components/products/Products'
-import Productsinfo from './components/products/Productsinfo'
 function App() {
   return (
     <>
-{/*     <Home />
+    <Home />
     <About />
-    <Contact /> */}
-       <Products />
-    <Productsinfo />
+    <Contact />
     </>
   )
 }
